7e033f1c19937d768242ccd35a5e4f9fe1a6eae6
Ludovic Rousseau <ludovic.rousseau+github@gmail.com>
2012-04-07 16:48:37 +0000

    Add support of variadic parameters for pam_prompt()
    Systems without pam_prompt() and using our own version of pam_prompt()
    can now use a variable number of parameters as on GNU/Linux.

--- src/pam_pkcs11/pam_pkcs11.c
+++ src/pam_pkcs11/pam_pkcs11.c
@@ -72,7 +72,7 @@ static int is_spaced_str(const char *str) {
 /*
  * implement pam utilities for older versions of pam.
  */
-static int pam_prompt(pam_handle_t *pamh, int style, char **response, char *text)
+static int pam_prompt(pam_handle_t *pamh, int style, char **response, char *fmt, ...)
 {
   int rv;
   struct pam_conv *conv;
@@ -81,6 +81,12 @@ static int pam_prompt(pam_handle_t *pamh, int style, char **response, char *text
   /* struct pam_message *(msgp[1]) = { &msg}; */
   struct pam_message *(msgp[1]);
   msgp[0] = &msg;
+  va_list va;
+  char text[128];
+
+  va_start(va, fmt);
+  vsnprintf(text, sizeof text, fmt, va);
+  va_end(va);
 
   msg.msg_style = style;
   msg.msg = text;
